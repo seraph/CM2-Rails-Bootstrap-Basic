0ad29e5903e1699deb2ae63b38d4fbdbcefb73f3
Kevin Francis <kfrancis@gmail.com>
2015-06-18 11:29:26 +0000

    Users now saved from sign_up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+    
+    if @user
+      flash[:success] = "Successfully created your account #{@user.name}"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
