8c9afd8785bf32c260ce5bdf9e3505967d95c78d
Kevin Francis <kfrancis@gmail.com>
2015-06-18 13:54:51 +0000

    Added user listing and edit functions for admins

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,24 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "User details updated"
+      redirect_to @user
+    else
+      flash[:danger] = "User details could not be saved"
+      render 'edit'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
